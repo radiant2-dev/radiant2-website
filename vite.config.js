4ee275d711feaf8f014725a93119ed42ff8d5c73
Maciej Ziaja <maciejzjg@gmail.com>
2026-01-27 09:08:12 +0100

    Setup github actions

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/radiant2-webiste/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
